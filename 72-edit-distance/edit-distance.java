a2f0a360ff3c656bb30d1f145ac5b723b4377e7e
akakhtar <121471337+akakhtar@users.noreply.github.com>
2024-08-05 20:17:30 +0530

    Time: 5 ms (65.94%) | Memory: 44.8 MB (73.49%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -16,10 +16,31 @@ class Solution {
         return dp[i][j] = ans;
     }
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
+        int dp[][] = new int[word1.length()+1][word2.length()+1];
         for(int[] arr : dp){
-            Arrays.fill(arr,-1);
+            Arrays.fill(arr,0);
         }
-        return solve(word1,word2,0,0,dp);
+        for(int j=0;j<word2.length();j++)
+         dp[word1.length()][j] = word2.length()-j;
+        for(int i=0;i<word1.length();i++)
+         dp[i][word2.length()] = word1.length()-i;
+
+         for(int i = word1.length()-1;i>=0;i--){
+            for(int j = word2.length()-1;j>=0;j--){
+                  int ans = 0;
+                if(word1.charAt(i) == word2.charAt(j))
+                  ans =  dp[i+1][j+1];
+                 
+                 else{
+              
+                int insertAns = 1 + dp[i][j+1];
+                int removeAns = 1 + dp[i+1][j];
+                int replaceAns = 1+dp[i+1][j+1];
+                ans = Math.min(insertAns,Math.min(removeAns,replaceAns));  
+            }
+            dp[i][j] = ans;
+            }
+         }  
+        return dp[0][0];
     }
 }

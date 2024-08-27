19d085714feb1dfabafe00f066e818560850569f
akakhtar <121471337+akakhtar@users.noreply.github.com>
2024-08-27 06:02:10 +0530

    Time: 12 ms (27.26%) | Memory: 45 MB (32.98%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -2,34 +2,24 @@ class Solution {
     public String reverseWords(String s) {
         Stack<String> st = new Stack<>();
         StringBuilder str = new StringBuilder();
-        
-        // Split the string by spaces and push non-empty words onto the stack
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == ' ') {
-                if (str.length() > 0) {
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                if(str.length()>0){
                     st.push(str.toString());
-                    str.setLength(0); // Reset the StringBuilder
+                    str.setLength(0);
                 }
-            } else {
-                str.append(ch);
             }
+            else 
+            str.append(s.charAt(i));
         }
-        
-        // Push the last word if there is one
-        if (str.length() > 0) {
-            st.push(str.toString());
+        if(str.length()>0)
+        st.push(str.toString());
+        String result = "";
+        while(!st.isEmpty()){
+            result += st.pop();
+            if(!st.isEmpty())
+            result += " ";
         }
-        
-        // Pop words from the stack to form the reversed string
-        StringBuilder result = new StringBuilder();
-        while (!st.isEmpty()) {
-            result.append(st.pop());
-            if (!st.isEmpty()) {
-                result.append(" ");
-            }
-        }
-        
-        return result.toString();
+        return result;
     }
 }

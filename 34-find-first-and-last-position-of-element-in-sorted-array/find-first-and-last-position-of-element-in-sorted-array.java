fafba76ef5b71a3604d43074864e1a61cb71c561
akakhtar <121471337+akakhtar@users.noreply.github.com>
2024-12-28 07:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (50.50%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,24 +1,6 @@
 class Solution {
 
-    int binarySearchFirst(int[] nums,int target){
-        int left = 0;
-        int right = nums.length-1;
-        while(left<=right){
-            int mid = left  + (right-left)/2;
-            if(nums[mid]>target)
-              right = mid-1;
-            else if(nums[mid]<target)
-              left = mid+1;
-             else{ 
-            if(mid==0 || nums[mid-1]!=nums[mid])
-              return mid;
-            else right = mid-1;  
-             }    
-        }
-        return -1;
-    }
-
-   int binarySearchLast(int[] nums,int target){
+   int binarySearch(int[] nums,int target,int first){
     int left = 0;
     int right = nums.length-1;
     while(left<=right){
@@ -28,18 +10,25 @@ class Solution {
         else if(nums[mid]<target)
         left = mid+1;
         else{
+        if(first == 0){        
         if(mid==nums.length-1 || nums[mid]!=nums[mid+1])
         return mid;
         else left = mid+1;
+        }
+        else{
+            if(mid==0 || nums[mid-1]!=nums[mid])
+              return mid;
+            else right = mid-1; 
+        }
         }
     }
     return -1;
    } 
 
     public int[] searchRange(int[] nums, int target) {
-        int x = binarySearchFirst(nums,target);
-        int y = binarySearchLast(nums,target);
+        int first = binarySearch(nums,target,1);
+        int last = binarySearch(nums,target,0);
 
-    return new int[]{x,y};    
+    return new int[]{first,last};    
     }
 }

fce53adc775e12a6b92b2c4083780a97b944d6c1
akakhtar <121471337+akakhtar@users.noreply.github.com>
2025-01-05 07:35:13 +0530

    Time: 8 ms (46.70%) | Memory: 74.3 MB (64.62%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int[] temp = new int[s.length()+1];
+        for(int i = 0;i<shifts.length;i++){
+            int start = shifts[i][0];
+            int end = shifts[i][1];
+            int direction = shifts[i][2] == 0?-1:1;
+
+            temp[start] += direction;
+            if(end+1 < temp.length) temp[end+1] -= direction;
+        }
+        int shiftValue = 0;
+        for(int i = 0;i<temp.length-1;i++){
+            shiftValue += temp[i];
+            temp[i] = shiftValue;
+        }
+        
+         StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int currentShift = temp[i] % 26; 
+            if (currentShift < 0) {
+                currentShift += 26; 
+            }
+            char shiftedChar = (char) ((s.charAt(i) - 'a' + currentShift) % 26 + 'a');
+            result.append(shiftedChar);
+        }
+        
+        return result.toString();
+    }
+}
